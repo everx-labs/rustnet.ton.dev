785ed527b4adecfffde118dfa4533ec53115717a
Artem Ryabov <artem.r@tonlabs.io>
2021-02-05 12:58:16 +0300

    Fixed ton-labs-node-tools build issue

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -36,8 +36,9 @@ rm -rf "${DOCKER_COMPOSE_DIR}/ton-node/build/ton-node"
 cd "${DOCKER_COMPOSE_DIR}/ton-node/build" && git clone --recursive "${TON_NODE_GITHUB_REPO}" ton-node
 cd "${DOCKER_COMPOSE_DIR}/ton-node/build/ton-node" && git checkout "${TON_NODE_GITHUB_COMMIT_ID}"
 
-cd "${DOCKER_COMPOSE_DIR}/ton-node/build/ton-node" && git clone --recursive "${TON_NODE_TOOLS_GITHUB_REPO}"
-cd "${DOCKER_COMPOSE_DIR}/ton-node/build/ton-node/ton-labs-node-tools" && git checkout "${TON_NODE_TOOLS_GITHUB_COMMIT_ID}"
+rm -rf "${DOCKER_COMPOSE_DIR}/ton-node/build/ton-labs-node-tools"
+cd "${DOCKER_COMPOSE_DIR}/ton-node/build" && git clone --recursive "${TON_NODE_TOOLS_GITHUB_REPO}"
+cd "${DOCKER_COMPOSE_DIR}/ton-node/build/ton-labs-node-tools" && git checkout "${TON_NODE_TOOLS_GITHUB_COMMIT_ID}"
 
 rm -rf "${DOCKER_COMPOSE_DIR}/ton-node/build/tonos-cli"
 cd "${DOCKER_COMPOSE_DIR}/ton-node/build" && git clone --recursive "${TONOS_CLI_GITHUB_REPO}"
